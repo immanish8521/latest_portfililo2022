d4a6046f6b47d1ae53fe1b27763d94fd4bdd022f
Manish Pathak <immanish8521@gmail.com>
2022-06-17 13:49:23 +0530

    deploy

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -11,9 +11,9 @@ export const projects = [
   },
   {
     id: 1,
-    title: "E-Commerce",
+    title: "Recipe App",
     description:
-      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+      "This is a Recipe App where user can find the recipe of different types of food using spoonacular Api.",
     image: "/images/2.png",
     tags: ["React", "JavaScript"],
     source: "https://google.com",
